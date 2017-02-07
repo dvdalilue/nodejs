b9a5214d771bc59b8b1e512035ee73ce6877692c
David Lilue <09-10444@gmail.com>
2017-02-06 22:53:49 -0400

    Cuando el producto cambia, se envía un correo

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var express = require('express');
+var firebase = require('firebase');
+var config = {
+    apiKey: "AIzaSyAo6t87dpnHv6q1vSIq1nR0JPhaTZ5O1xg",
+    authDomain: "test-krono.firebaseapp.com",
+    databaseURL: "https://test-krono.firebaseio.com",
+    storageBucket: "test-krono.appspot.com",
+    messagingSenderId: "842158548684"
+  };
+firebase.initializeApp(config);
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -15,4 +24,30 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+firebase.database().ref('productos').on("child_changed", function(snapshot) {
+    const nodemailer = require('nodemailer');
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'krononodejs@gmail.com',
+            pass: 'nodejskrono'
+        }
+    });
+
+    let mailOptions = {
+        from: '"KronoApp" <no-reply@test-krono.com>', // sender address
+        to: 'dvdalilue@gmail.com', // list of receivers
+        subject: '[Krono] Producto cambiado: \'' + snapshot.val().nombre + '\'', // Subject line
+        html: 'El Producto \'' + snapshot.val().nombre + '\' ha sido modificado' // html body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
 });
